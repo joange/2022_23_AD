6ada72ac1e7f74df975ca86aa43fe61a11fe10e4
Joan Gerard Camarena <jgcamarena@ieseljust.com>
2022-10-06 11:44:24 +0200

    Ejercicio Resuelto JSON

--- TestJson/src/main/java/ExerciseJSON/App.java
+++ TestJson/src/main/java/ExerciseJSON/App.java
@@ -4,14 +4,18 @@
  */
 package ExerciseJSON;
 
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.FileReader;
+import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
 import javax.xml.transform.TransformerException;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
@@ -20,6 +24,7 @@ import org.json.JSONArray;
 import org.json.JSONObject;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
+import org.xml.sax.SAXException;
 
 /**
  *
@@ -48,82 +53,146 @@ public class App {
             return null;
         }
     }
-    
-    private void withoutVehicles(JSONObject objeto){
-        JSONArray personatges= objeto.getJSONArray("personatges");
-        
-        for (int i = 0; i < personatges.length(); i++) {
-            JSONObject personatge=personatges.getJSONObject(i);
-            
-            //extract the vehicles
-            JSONArray vehicles=personatge.getJSONArray("vehicles");
-            
-            String name=personatge.getString("name");
-            
-            if (vehicles.length()==0)
-                System.out.println("El personatge " + name + " no ha conduït");
-            
-        }
+
+    public Document OpenXML(String name) throws IOException, SAXException, ParserConfigurationException, FileNotFoundException {
+        // Create an instance of DocumentBuilderFactory
+        DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+        // Using the DocumentBuilderFactory instance we create a DocumentBuilder
+        DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+        //And we use DocumentBuilder's "parse" method to get the document
+        Document doc = dBuilder.parse(new File(name));
+
+        return doc;
     }
+
+    public Document NewXML() throws IOException, SAXException, ParserConfigurationException, FileNotFoundException {
+        // Create an instance of DocumentBuilderFactory
+        DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+        // Using the DocumentBuilderFactory instance we create a DocumentBuilder
+        DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+        //And we use DocumentBuilder's "parse" method to get the document
+        Document doc = dBuilder.newDocument();
+
+        return doc;
+    }
+
     
+    
+    private void writeXMLFile(Document doc, String filename) {
+        try {
+            
+            Transformer trans = TransformerFactory.newInstance().newTransformer();
+            DOMSource source = new DOMSource(doc);
+            StreamResult result = new StreamResult(new FileOutputStream(filename));
+            trans.transform(source, result);
+            
+        } catch (TransformerConfigurationException ex) {
+            Logger.getLogger(App.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(App.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (TransformerException ex) {
+            Logger.getLogger(App.class.getName()).log(Level.SEVERE, null, ex);
+        } 
+    }
+
+    private void withoutVehicles(JSONObject objeto) {
+
+        JSONArray personatges = objeto.getJSONArray("personatges");
+
+        for (int i = 0; i < personatges.length(); i++) {
+            JSONObject personatge = personatges.getJSONObject(i);
+
+            JSONArray vehicles = personatge.getJSONArray("vehicles");
+
+            if (vehicles.length() == 0) {
+                String name = personatge.getString("name");
+                System.out.println(name + " hasn't drove a vehicle");
+            }
+        }
+
+    }
+
     /*
     <character films="4" vehicles="0"> 
         <name>Luke Skywalker</name> 
         <mass>77</mass> 
         <url>https://swapi.dev/api/people/1/</url>
     </character>
-    *
-    */
-    private void saveXMLFromJSON(JSONObject objeto) {
-        
+     */
+    private void saveXMLFromJSON(JSONObject objeto, String filename) {
         try {
-            JSONArray personatges= objeto.getJSONArray("personatges");
-            
-            Document doc=DocumentBuilderFactory.newInstance().newDocumentBuilder().newDocument();
-            Element root=doc.createElement("Personatges");
+            Document doc = NewXML();
+
+            Element root = doc.createElement("personatges");
             doc.appendChild(root);
-            
+
+            JSONArray personatges = objeto.getJSONArray("personatges");
+
             for (int i = 0; i < personatges.length(); i++) {
-                JSONObject personatgeJSON=personatges.getJSONObject(i);
+                JSONObject personatge = personatges.getJSONObject(i);
+
+                Element character=doc.createElement("character");
                 
-                Element personatgeXML=doc.createElement("character");
+                JSONArray films=personatge.getJSONArray("films");
+                JSONArray vehicles=personatge.getJSONArray("vehicles");
                 
-                JSONArray vehicles=personatgeJSON.getJSONArray("vehicles");
-                personatgeXML.setAttribute("vehicles", String.valueOf(vehicles.length()));
+                character.setAttribute("films", String.valueOf(films.length()));
+                character.setAttribute("vehicles", vehicles.length()+"");
                 
-                JSONArray films=personatgeJSON.getJSONArray("films");
-                personatgeXML.setAttribute("films", String.valueOf(films.length()));
-               
-                root.appendChild(personatgeXML);
+                // creo el name
+                String name=personatge.getString("name"); 
+                Element nombre=doc.createElement("name");
+                nombre.appendChild(doc.createTextNode(name));
+                character.appendChild(nombre);
+                
+                //creo el mass
+                String mass=personatge.getString("mass"); 
+                Element peso=doc.createElement("mass");
+                peso.appendChild(doc.createTextNode(mass));
+                character.appendChild(peso);
+                
+                //creo el link
+                String url=personatge.getString("url"); 
+                Element link=doc.createElement("url");
+                link.appendChild(doc.createTextNode(url));
+                character.appendChild(link);
+                
+                root.appendChild(character);
+                
+                
+                
                 
             }
             
-            System.out.println(doc.toString());
-            
-             Transformer trans = TransformerFactory.newInstance().newTransformer();
-            DOMSource source = new DOMSource(doc);
-            StreamResult result = new StreamResult(new FileOutputStream("SW.xml"));
-            trans.transform(source, result);
-            
+            writeXMLFile(doc, filename);
+
+        } catch (IOException ex) {
+            Logger.getLogger(App.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SAXException ex) {
+            Logger.getLogger(App.class.getName()).log(Level.SEVERE, null, ex);
         } catch (ParserConfigurationException ex) {
             Logger.getLogger(App.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (FileNotFoundException ex) {
-            Logger.getLogger(App.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (TransformerException ex) {
-            Logger.getLogger(App.class.getName()).log(Level.SEVERE, null, ex);
         }
+
     }
 
     public static void main(String[] args) {
-        App app= new App();
-        
-        JSONObject objeto= app.LligJSON("SW.json");
-        
-//        System.out.println(objeto);
+
+//        JSONObject obj=new JSONObject();
+//        
+//        obj.put("name", "Joan Gerard");
+//        obj.put("age", 48);
+//        System.out.println(obj.toString(2));
+//        
+        App app = new App();
+
+        JSONObject objeto = app.LligJSON("SW.json");
+
+        System.out.println(objeto.toString(2));
 
         app.withoutVehicles(objeto);
-        System.out.println("===============================");
-        
-        app.saveXMLFromJSON(objeto);
+
+//        System.out.println("===============================");
+        app.saveXMLFromJSON(objeto,"SW.xml");
     }
 }
